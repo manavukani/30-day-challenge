3d21ab16f2c6b9bc09ce04db1c52328a6295653e
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-07-14 11:36:08 +0530

    day 1

---
+++ day_1.js
@@ -0,0 +1,37 @@
+// task 1
+var num = 1;
+console.log(num);
+
+// task 2
+let str = "Hello World";
+console.log(str);
+
+// task 3
+let bool = 1;
+console.log(bool);
+
+// task 4
+var a = 4;
+var b = "Hello";
+var c = { name: "John", age: 30 };
+var d = true;
+var e = [1, 2, 3];
+
+console.log(
+  typeof a,
+  typeof b,
+  typeof c,
+  typeof d,
+  typeof e
+);
+
+// task 5
+let first_name = "John";
+console.log(first_name);
+first_name = "Mark";
+console.log(first_name);
+
+// task 6
+const last_name = "Doe"
+// last_name = "Smith";
+console.log(last_name);
