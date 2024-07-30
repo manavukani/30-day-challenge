93feea40f258bdc8d2bef72d3b6d0d5ddaf6cca0
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-07-30 21:31:14 +0530

    updated merge sort

--- Day 18/Sorting Algorithms/mergeSort.js
+++ Day 18/Sorting Algorithms/mergeSort.js
@@ -38,3 +38,20 @@ const merge = (left, right) => {
 
 const arr = [64, 34, 25, 12, 22, 11, 90];
 console.log(mergeSort(arr));
+
+// another way to write merge
+function merge2(left, right) {
+  let sortedArr = []; // the sorted items will go here
+  while (left.length && right.length) {
+    // Insert the smallest item into sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  // Use spread operators to create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right];
+}
+
+console.log(merge2([1, 4], [2, 6, 9])); // [1, 2, 4, 6, 9]
