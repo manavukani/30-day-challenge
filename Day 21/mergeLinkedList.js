fd5da3f149764201a83ba763b1c7e2f2ea704336
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-08-04 16:31:24 +0530

    leetcode easy done

--- Day 21/mergeLinkedList.js
+++ Day 21/mergeLinkedList.js
@@ -8,65 +8,49 @@ Create a few test cases with linked lists and log the merged list.
 
 */
 
-                            
-// Node class represents a
-// node in a linked list
 class Node {
-    // Data stored in the node
-    constructor(data1, next1 = null) {
-        this.data = data1;
-        this.next = next1;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-// Function to merge two sorted linked lists
-function sortTwoLinkedLists(list1, list2) {
-    // Create a dummy node to serve
-    // as the head of the merged list
-    const dummyNode = new Node(-1);
-    let temp = dummyNode;
+const sortTwoLinkedLists = (list1, list2) => {
+  // dummy node - head of the merged list
+  const dummyNode = new Node(-1);
+  let temp = dummyNode;
 
-    // Traverse both lists simultaneously
-    while (list1 !== null && list2 !== null) {
-        // Compare elements of both lists and
-        // link the smaller node to the merged list
-        if (list1.data <= list2.data) {
-            temp.next = list1;
-            list1 = list1.next;
-        } else {
-            temp.next = list2;
-            list2 = list2.next;
-        }
-        // Move the temporary pointer
-        // to the next node
-        temp = temp.next;
-    }
-
-    // If any list still has remaining
-    // elements, append them to the merged list
-    if (list1 !== null) {
-        temp.next = list1;
+  while (list1 !== null && list2 !== null) {
+    if (list1.data <= list2.data) {
+      temp.next = list1;
+      list1 = list1.next;
     } else {
-        temp.next = list2;
+      temp.next = list2;
+      list2 = list2.next;
     }
-    // Return the merged list starting
-    // from the next of the dummy node
-    return dummyNode.next;
-}
+    // Move temporary to next node
+    temp = temp.next;
+  }
 
-// Function to print the linked list
-function printLinkedList(head) {
-    let temp = head;
-    while (temp !== null) {
-        // Print the data of the current node
-        process.stdout.write(temp.data + " ");
-        // Move to the next node
-        temp = temp.next;
-    }
-    console.log();
-}
+  // remaining nodes in any list
+  if (list1 !== null) {
+    temp.next = list1;
+  } else if (list2 !== null) {
+    temp.next = list2;
+  }
 
-// Example Linked Lists
+  return dummyNode.next;
+};
+
+const printLinkedList = (head) => {
+  let temp = head;
+  while (temp !== null) {
+    process.stdout.write(temp.data + " ");
+    temp = temp.next;
+  }
+  console.log();
+};
+
 let list1 = new Node(1);
 list1.next = new Node(3);
 list1.next.next = new Node(5);
--- Day 21/validParentheses.js
+++ Day 21/validParentheses.js
@@ -9,63 +9,52 @@ Log the result for a few test cases.
 
 */
 
-function areBracketsBalanced(expr)
-{
-    
-    // Using ArrayDeque is faster
-    // than using Stack class
-    let stack = [];
+const areBracketsBalanced = (expr) => {
+  let stack = [];
 
-    // Traversing the Expression
-    for(let i = 0; i < expr.length; i++)
-    {
-        let x = expr[i];
+  for (let i = 0; i < expr.length; i++) {
+    let x = expr[i];
 
-        if (x == '(' || x == '[' || x == '{')
-        {
-            
-            // Push the element in the stack
-            stack.push(x);
-            continue;
-        }
-
-        // If current character is not opening
-        // bracket, then it must be closing. 
-        // So stack cannot be empty at this point.
-        if (stack.length == 0)
-            return false;
-            
-        let check;
-        switch (x){
-        case ')':
-            check = stack.pop();
-            if (check == '{' || check == '[')
-                return false;
-            break;
-
-        case '}':
-            check = stack.pop();
-            if (check == '(' || check == '[')
-                return false;
-            break;
-
-        case ']':
-            check = stack.pop();
-            if (check == '(' || check == '{')
-                return false;
-            break;
-        }
+    // push to stack
+    if (x == "(" || x == "[" || x == "{") {
+      stack.push(x);
+      continue;
     }
 
-    // Check Empty Stack
-    return (stack.length == 0);
-}
+    // If current character is not opening
+    // bracket, then it must be closing.
+    // So stack cannot be empty at this point.
+    if (stack.length == 0) return false;
 
-// Driver code
-let expr = "([{}])";
+    let check;
 
-// Function call
-if (areBracketsBalanced(expr))
-    document.write("Balanced ");
-else
-    document.write("Not Balanced ");
+    // pop
+    switch (x) {
+      case ")":
+        check = stack.pop();
+        if (check == "{" || check == "[") return false;
+        break;
+
+      case "}":
+        check = stack.pop();
+        if (check == "(" || check == "[") return false;
+        break;
+
+      case "]":
+        check = stack.pop();
+        if (check == "(" || check == "{") return false;
+        break;
+    }
+  }
+
+  // check if emptied
+  return stack.length == 0;
+};
+
+let expr1 = "([([()]){})]";
+let expr2 = "([[()]{}])";
+
+if (areBracketsBalanced(expr1)) console.log(expr1, ": Balanced ");
+else console.log(expr1, ": Not Balanced ");
+if (areBracketsBalanced(expr2)) console.log(expr2, ": Balanced ");
+else console.log(expr2, ": Not Balanced ");
